4d2ea4fbf62f74614c8f7841b7fe26b11e0c9bd4
Sivaram <sivaramsalivahan@gmail.com>
2020-11-29 10:52:07 +0530

    Adding More Functions

--- StringExample.java
+++ StringExample.java
@@ -5,6 +5,10 @@ public class StringExample {
         //String functions
         //String length
         System.out.println(s.length());
+        //concat
+        String s1="Hello";
+        String s2="world";
+        System.out.println(s1.concat(" ").concat(s2));
 
     }
 }
